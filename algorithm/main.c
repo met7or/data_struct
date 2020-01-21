182d2c9cdcb9e3078f9e81a5c7dfc12736cb5c69
xinyu.wu <xinyu.wu@ly.com>
2020-01-21 11:18:14 +0800

    Initial Commit

---
+++ algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  algorithm
+//
+//  Created by met7or on 2020/1/21.
+//  Copyright © 2020 met7or. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
